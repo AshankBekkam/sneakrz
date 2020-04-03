a46f98747d257115b77808255b5026e3bfe991d5
ashank <ashankbekkam@gmail.com>
2020-04-03 15:06:37 +0530

    first commit

---
+++ src/components/homepage/Homepage.js
@@ -0,0 +1,47 @@
+import React from "react"
+
+import "./Homepage.scss"
+
+
+const Homepage = ()=>(
+
+    <div className = 'homepage'>
+        <div className = 'directory-menu'>
+            <div className = 'menu-item'>
+                <div className = 'content'>
+                    <h1 className = 'title'>SNEAKERS</h1>
+                    <h2 className = 'subtitle'>SHOP NOW</h2>
+                </div>
+            </div>
+            <div className = 'menu-item'>
+                <div className = 'content'>
+                    <h1 className = 'title'>RUNNING SHOES</h1>
+                    <h2 className = 'subtitle'>SHOP NOW</h2>
+                </div>
+            </div>
+            <div className = 'menu-item'>
+                <div className = 'content'>
+                    <h1 className = 'title'>FOOTBALL SHOES </h1>
+                    <h2 className = 'subtitle'>SHOP NOW</h2>
+                </div>
+            </div>
+            <div className = 'menu-item'>
+                <div className = 'content'>
+                    <h1 className = 'title'>MENS</h1>
+                    <h2 className = 'subtitle'>SHOP NOW</h2>
+                </div>
+            </div>
+            <div className = 'menu-item'>
+                <div className = 'content'>
+                    <h1 className = 'title'>WOMENS</h1>
+                    <h2 className = 'subtitle'>SHOP NOW</h2>
+                </div>
+            </div>
+
+        </div>
+
+
+    </div>
+)
+
+export default Homepage
